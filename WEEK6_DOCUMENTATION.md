aeaae12db2fe2986cd9a942149510053525da656
mimo-owl <m-shirasaka@g.ecc.u-tokyo.ac.jp>
2025-07-03 21:52:23 +0900

    completed week6 hw

--- WEEK6_DOCUMENTATION.md
+++ WEEK6_DOCUMENTATION.md
@@ -1,18 +1,27 @@
 ## STEP Week6 Documentation
 ### Author: Mimo Shirasaka
 
-## Challenge
-巡回セールスマン問題を解く。
-ランダムとgreedy 探索により経路を求めるプログラムはベースラインとして提供されている。
-総合経路長を求めるプログラムは `output_vefifier.py`
-ベースラインよりも良いスコアを出せるアルゴリズムを実装して、output_i.csv (i = 0 ~ 6)を書き換え、`output_verifier.py`を実行してスコアを確認しよう。
-先週より高いスコアを目指す。
+## Challenge 2
+Week5 の巡回セールスマン問題 (TSP) のプログラムを改善して、Challenge 6 のベストスコア更新を目指す
+(Challenge 6 には 40000 以下の答えが存在します)
 
-## 私のプログラムの実行結果（as of 2025.6.25）
+### やったこと
+1. コードレビューした他のメンバーが、ILS を試していたので、その関数を少し自分のプログラム用に編集して試してみた。
+結果：自分のコードでは、値としては改善しなかったが、元コードで出力がわかりやすいように工夫されていたので、実行すると何が起きているのかわかり、良かった。
+
 他の人と組み合わせた実行結果（2opt の後に ILS）：
 ILS finished. Final Best Distance: 20947.12
 
-オリジナルの工夫の実行結果：
+2. エリアを４分割してから、greedy + 2opt する方法
+
+プログラム： `solver_area2opt.py`
+
+内容：
+エリアを４分割
+エリアごとに、greedy + 2opt で経路最適化
+全ての順列パターンを試す（4! = 24通り）し、最短経路になる順序で４エリアを連結
+
+結果：
 
 Challenge 5
 output          :   24123.07
@@ -20,16 +29,91 @@ sample/random   :  347392.97
 sample/greedy   :   25331.84
 sample/sa       :   21119.55
 
+この解法だと、week5より長くなってしまった…。
 
-## 実装の説明
-実装は、`solver_area2opt.py`に行いました。
-内容:
 
-エリアを４分割
-エリアごとに、greedy + 2opt で経路最適化
-全ての順列パターンを試す（4! = 24通り）し、最短経路になる順序で４エリアを連結
+2. 2optの時にルートが改善したかどうかの判定を、全体ルートではなく、着目する２ノード間のみ計算するよう変更した
+プログラム：`solver_opt`
+結果：実行時間が格段と速くなった！（week5 の時は、input6 で2日くらいかかったが、今回は１時間くらいでおわった）
 
-という方法をとってみました。
+Challenge 6
+output          :   42712.37
+sample/random   : 1374393.14
+sample/greedy   :   49892.05
+sample/sa       :   44393.89
+
+ここで、改善度合いを可視化して調べてみたところ、大きな改善が少ないことに気づいた。　
+
+![2-opt improvement graph](two_opt_inp6.png)
+
+なんとか改善が大きいものを優先して計算したい！
+ということで、下記３番に示すやり方を思いつき、試してみた。
+
+
+3. 2optを、改善度合いの大きさが大きいものを優先して計算するようにした
+プログラム：`solver_opt_week6.py`
+
+#### プログラムの流れ
+準備：まずgreedyで経路を求める
+2opt 1回目：改善度合いの大きさに関する閾値を設定し、2ノード間のswapをした結果、短くなった度合いが大きいもののみ、更新する
+2opt 2回目：改善したものは全て、best_route として更新する
+
+#### 結果
+実行がとても速くなった！（１、2分で実行完了した。）
+
+Challenge 6
+output          :   41711.62
+sample/random   : 1374393.14
+sample/greedy   :   49892.05
+sample/sa       :   44393.89
+
+
+![2-opt improvement graph](two_opt_85-100_inp6.png)
+
+２段階で2optすることで、スコアも改善した。
+また、プロットしてみた結果、改善度合いが大きいものが増えたことがわかった。
+
+4. 3では2段階だったが、それを１０段階に分けて、だんだん閾値を10ずつ緩めていった
+
+Iteration 10: Tour length = 42712.37
+Result has been saved to: output_6.csv
+
+![2-opt improvement graph](multi_opt_inp6.png)
+
+２段階の時の方が結果は良かった。
+
+# Challenge 3
+input_7.csv で試してみよう！
+
+### やったこと
+1. Challenge 2 の、解法3で解いてみた。
+
+結果：
+
+Challenge 7
+output          :   83156.14
+
+![2-opt improvement graph](two_opt_85-100_inp7.png)
+
+2. 閾値を１０段階に分けて、10回実行した
+
+Iteration 1: Tour length = 95983.29
+Iteration 2: Tour length = 95983.29
+Iteration 3: Tour length = 95983.29
+Iteration 4: Tour length = 95983.29
+Improvement found: 52.0739 -> 26.0036 (cities 7648, 4022, 7557, 3536), shorted by: 49.94%
+Iteration 5: Tour length = 95957.22
+Improvement found: 21.3522 -> 12.0736 (cities 5219, 3602, 3786, 1874), shorted by: 56.54%
+Improvement found: 43.2959 -> 23.9003 (cities 1895, 6016, 141, 6846), shorted by: 55.20%
+Improvement found: 65.2878 -> 38.8338 (cities 1325, 7528, 3788, 5158), shorted by: 59.48%
+Improvement found: 50.0950 -> 28.6836 (cities 1437, 1344, 3462, 2728), shorted by: 57.26%
+...
+Iteration 7: Tour length = 91581.71
+...
+
+結果：
+現在実行中（だが、Challenge2 の結果に基づくと、2段階の時よりも長くなってしまうかもしれない）
+
 
 
 実行方法：
@@ -40,7 +124,7 @@ python solver_area2opt.py input_0.csv
 結果は、`output_0.csv` に保存されます。
 
 
-## 結果の考察
+
 
 
 
---
+++ solver_opt_week6.py
@@ -0,0 +1,128 @@
+# solver_2opt.py
+import sys
+import math
+import os
+import matplotlib.pyplot as plt
+
+from common import print_tour, read_input, format_tour
+import solver_greedy
+
+def total_distance(tour, cities):
+    total_dist = 0
+    num_cities = len(tour)
+    for i in range(num_cities):
+        j = (i + 1) % num_cities
+        total_dist += distance(cities[tour[i]], cities[tour[j]])
+    return total_dist
+
+def distance(city1, city2):
+    return math.sqrt((city1[0] - city2[0]) ** 2 + (city1[1] - city2[1]) ** 2)
+
+def two_opt_swap(route, i, k):
+    new_route  = route[:i] + route[i:k+1][::-1] + route[k+1:] # iより前と、kより後は変更なし。i~kの部分だけ、順番を反転させる
+    return new_route
+
+# def two_opt(tour, cities):
+#     num_cities = len(cities)
+#     best_route = tour[:]
+#     can_improve = True
+
+#     # 改善の余地がある限り、次の処理を実行
+#     while can_improve:
+#         can_improve = False
+#         for i in range(1, num_cities - 1):
+#             for j in range(i + 1, num_cities):
+#                 dist_ij = distance(cities[best_route[i]], cities[best_route[j]])
+#                 print(f"Distance between city {best_route[i]} and city {best_route[j]}: {dist_ij:.4f}")
+#                 # 現時点でのベストルートについて、ノードiとjの間のルートを反転させる
+#                 new_route = two_opt_swap(best_route, i, j)
+#                 # swap前後で総距離を比較し、より短くなっていたらそれをベストルートとして更新
+#                 if total_distance(new_route, cities) < total_distance(best_route, cities):
+#                     best_route = new_route[:]
+#                     can_improve = True
+#         tour = best_route[:]
+#     return best_route
+
+def two_opt(tour, cities, threshold=100):
+    num_cities = len(cities)
+    best_route = tour[:]
+    can_improve = True
+
+    # improvements_euclid = []  # 直線距離横軸
+    improvements_path = []    # ルート距離横軸
+
+    while can_improve:
+        can_improve = False
+        for i in range(1, num_cities - 1):
+            for j in range(i + 1, num_cities):
+
+                # 現時点でのベストルートについて、ノードiとjの間のルートを反転させる
+                a = best_route[i - 1]
+                b = best_route[i]
+                c = best_route[j]
+                d = best_route[(j + 1) % num_cities]
+
+                before = distance(cities[a], cities[b]) + distance(cities[c], cities[d])
+                after  = distance(cities[a], cities[c]) + distance(cities[b], cities[d])
+
+                # 反転した結果が改善されていればベストルートとして更新
+                if after < before:
+                    improvement_ratio = after / before * 100
+                    if threshold != 100:
+                        if improvement_ratio > threshold:
+                            continue
+                    print(f"Improvement found: {before:.4f} -> {after:.4f} (cities {a}, {b}, {c}, {d}), shorted by: {improvement_ratio:.2f}%")
+                    new_route = two_opt_swap(best_route, i, j)
+                    best_route = new_route[:]
+                    can_improve = True
+
+                    # improvement_ratio = after / before * 100
+                    path_width = before  # i, j 間の元のルートの長さ
+
+                    if len(improvements_path) < 1000:
+                        improvements_path.append( (path_width, improvement_ratio) )
+
+        tour = best_route[:]
+
+    return best_route, improvements_path
+
+
+if __name__ == '__main__':
+    assert len(sys.argv) > 1
+    input_file_path  = sys.argv[1]
+    input_filename = os.path.basename(input_file_path)
+    challenge_number = input_filename.split("_")[1].split(".")[0]
+
+    # greedy で解く
+    greedy_tour = solver_greedy.solve(read_input(sys.argv[1]))
+    print_tour(greedy_tour)
+
+    # greedy で求めた経路に2-optを適用
+
+    # 1回目は改善度合いが大きいものだけを対象にする
+    two_opt_limited_tour, improvements_path = two_opt(greedy_tour, read_input(sys.argv[1]), threshold=85)
+    # 2回目は改善度合いが小さいものも含めて
+    two_opt_full_tour, improvements_path = two_opt(two_opt_limited_tour, read_input(sys.argv[1]), threshold=100)
+
+
+    # optimized_tour, improvements_path = two_opt(tour, read_input(sys.argv[1]))
+    # print_tour(optimized_tour)
+
+    output_file_path = f'output_{challenge_number}.csv'
+    # output_file_path = f'output_8.csv'
+    with open(output_file_path, 'w') as f:
+        f.write(format_tour(two_opt_full_tour) + '\n')
+
+    print(f"Result has been saved to: {output_file_path}")
+
+    if improvements_path:
+        x2 = [w for (w, r) in improvements_path]
+        y2 = [r for (w, r) in improvements_path]
+        print(f"y2: {y2}")
+        plt.figure()
+        plt.scatter(x2, y2, alpha=0.7)
+        plt.xlabel("Route distance between i and j (before swap)")
+        plt.ylabel("Improved distance (% of original route distance)")
+        plt.title("First 500 improvements - Route distance as x-axis")
+        plt.grid()
+        plt.show()
